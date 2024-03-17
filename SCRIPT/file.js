ecc0c76b044558f0e92806529054b88ea3c27d4a
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-03-17 12:48:09 +0100

    Cells now get a random color

--- SCRIPT/file.js
+++ SCRIPT/file.js
@@ -1,7 +1,8 @@
 const gridContainer = document.querySelector('#container');
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName('cell');
-let numberOfCells
+let numberOfCells;
+let randomColor;
 
 const inputGrid = document.querySelector('#inputGrid');
 inputGrid.addEventListener('click', () => {
@@ -23,16 +24,24 @@ function makeRows(rowNum) {
 };
 
 function makeColumns(cellNum) {
-    //Creates columns
+
+    // Creates columns
     for (let c = 0; c < rows.length; c++) {
         for (let i = 0; i < cellNum; i++) {
             let cell = document.createElement('div');
             cell.className = 'cell';
+            cell.style.minHeight = (700 / numberOfCells) + 'px';
+            cell.style.minWidth = (700 / numberOfCells) + 'px';
+            cell.style.border = `1px solid black`;
             rows[c].appendChild(cell);
         };
     };
 };
-
+function RGB() {
+    const colors = ['red', 'green', 'blue']
+    var randomNumber = Math.floor(Math.random() * colors.length); // Generate a random number between 0 and 2
+    return colors[randomNumber];
+};
 
 function changeCells() {
     while (gridContainer.firstChild) {
@@ -44,7 +53,7 @@ function changeCells() {
     //Colors in the cells
     for (let j = 0; j < cells.length; j++) {
         cells[j].addEventListener('mouseover', () =>{
-            cells[j].style.backgroundColor = 'red';
+            cells[j].style.backgroundColor = RGB();
         });    
     };
 };
