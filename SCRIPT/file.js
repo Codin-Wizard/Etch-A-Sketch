6db485097cde8a7143e4b54a792738b37b4adee9
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-03-17 11:01:32 +0100

    Added CSS fro the grid

--- SCRIPT/file.js
+++ SCRIPT/file.js
@@ -1,12 +1,13 @@
 const gridContainer = document.querySelector('#container');
 let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+
 
 function makeRows(rowNum) {
     //Creates rows
     for (let r = 0; r < rowNum; r++) {
         let row = document.createElement('div');
-        gridContainer.appendChild(row).className('gridRow');     
+        row.className ='gridRow'; 
+        gridContainer.appendChild(row);    
     };
 };
 
@@ -15,7 +16,15 @@ function makeColumns(cellNum) {
     for (let c = 0; c < rows.length; c++) {
         for (let i = 0; i < cellNum; i++) {
             let cell = document.createElement('div');
-            rows[i].appendChild(cell).className('cell');
+            cell.className = 'cell';
+            rows[c].appendChild(cell);
         };
     };
 };
+function defaultGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+defaultGrid();
+
