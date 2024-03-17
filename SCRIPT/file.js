7b3b61e96869d6bf6551023cca458ae52ad30882
Codin-Wizard <139460738+Codin-Wizard@users.noreply.github.com>
2024-03-17 11:44:32 +0100

    Nothing

--- SCRIPT/file.js
+++ SCRIPT/file.js
@@ -2,6 +2,12 @@ const gridContainer = document.querySelector('#container');
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName('cell');
 
+const inputGrid = document.querySelector('#inputGrid');
+inputGrid.addEventListener('click', () => {
+    let numberOfCells = parseFloat(prompt('How many cells do you want?'));
+});
+
+
 function makeRows(rowNum) {
     //Creates rows
     for (let r = 0; r < rowNum; r++) {
@@ -24,10 +30,10 @@ function makeColumns(cellNum) {
 function defaultGrid() {
     makeRows(16);
     makeColumns(16);
-}
-
+}; 
 defaultGrid();
 
+//Colors in the cells
 for (let j = 0; j < cells.length; j++) {
     cells[j].addEventListener('mouseover', () =>{
         cells[j].style.backgroundColor = 'red';
